c1df6cfe127f1194df545b6ab218eca06a98e4ad
kolya112 <nikolayurch@gmail.com>
2023-12-15 12:37:47 +0300

    TCP server test

---
+++ GMICLI/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMICLI
+{
+    internal class Server
+    {
+        internal static async Task Start()
+        {
+            TcpListener tcpServer = new TcpListener(IPAddress.Any, 0);
+            tcpServer.Start();
+            await Receive(tcpServer);
+
+        }
+
+        internal static async Task Receive(TcpListener tcp)
+        {
+            while (true)
+            {
+                using var tcpClient = await tcp.AcceptTcpClientAsync();
+                Console.WriteLine($"Входящее подключение: {tcpClient.Client.RemoteEndPoint}");
+            }
+        }
+    }
+}
