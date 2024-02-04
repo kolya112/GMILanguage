d526e05ed6598836a63ce1d74766f64fabef9bd7
antoxarbl6ak <antonmilonow3@gmail.com>
2024-02-04 23:49:28 +0300

    изменения
    1. поменял интерфейс и добавил окно для вывода и исключений
    2. написал кнопки для работы с текстовыми файлами
    3. структурное изменение интерфейса(раньше загружал в объект, теперь в экземпляр класса)
    4. начал связывать окно с вводом с окном робота

--- src/IDE/2.py
+++ src/IDE/2.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtGui import QPainter, QPen, QImage
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QTextEdit, QMessageBox
+from PyQt5.QtGui import QPainter, QPen, QImage, QIcon
 from PyQt5.QtCore import Qt, QPointF, QTimer
 from PyQt5 import uic
 
@@ -16,10 +16,12 @@ class Window(QMainWindow):
         self.t = QTimer(self)
         self.t.timeout.connect(self.te)
         self.img = QImage("robot.png")
+        self.setWindowIcon(QIcon("icon.png"))
         self.setGeometry(700, 300, 630, 630)
         self.setWindowTitle("Surface")
         self.setMaximumSize(630, 630)
         self.setMinimumSize(630, 630)
+        self.file = ""
         self.x = 0
         self.y = 0
         self.v = 30
@@ -66,15 +68,80 @@ class Window(QMainWindow):
     def stop(self):
         self.t.stop()
 
+class Textui(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        uic.loadUi("textedit.ui", self)
+        self.fl = True
+        self.file = ""
 
+        self.setWindowIcon(QIcon("icon.png"))
+        self.actionRun.triggered.connect(self.run)
+        self.actionStop.triggered.connect(self.stop)
+        self.actionSave.triggered.connect(self.save)
+        self.actionSave_as.triggered.connect(self.saveas)
+        self.actionOpen.triggered.connect(self.open)
+        self.textedit.textChanged.connect(self.change)
+        self.show()
+    def change(self):
+        self.fl = True
+    def run(self):
+        if self.fl == False:
+            w.run()
+        else:
+            wanna = QMessageBox()
+            wanna.setText("Ваш код должен быть сохранен перед запуском. \n"
+                          "\t\tOk чтобы сохранить")
+            wanna.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            wanna
+            ret = wanna.exec()
+            if ret == QMessageBox.Ok:
+                self.save()
+    def stop(self):
+        w.stop()
+    def cont(self):
+        pass
+    def save(self):
+        if self.file:
+            with open(self.file, "w") as of:
+                of.write(self.textedit.toPlainText())
+        else:
+            self.saveas()
+        self.fl = False
+    def open(self):
+        fod = QFileDialog()
+        fod.setFileMode(QFileDialog.AnyFile)
+        fod.setWindowTitle("open")
+        fod.setNameFilter("text files (*.txt)")
+        fod.setViewMode(QFileDialog.Detail)
+        if fod.exec_():
+            self.file = fod.selectedFiles()[-1]
+            if self.file[-4:] != ".txt":
+                self.file += ".txt"
+            try:
+                with open(self.file, 'r') as f:
+                    t = f.read()
+                    self.textedit.setText(t)
+            except FileNotFoundError:
+                pop = QMessageBox()
+                pop.setText("Не удалось найти этот файл")
+                pop.setWindowTitle("Ошибка")
+                pop.setStandardButtons(QMessageBox.Ok)
+                pop.exec_()
+    def saveas(self):
+        fsd = QFileDialog()
+        fsd.setFileMode(QFileDialog.AnyFile)
+        fsd.setWindowTitle("save as")
+        fsd.setNameFilter("text files (*.txt)")
+        fsd.setViewMode(QFileDialog.Detail)
+        if fsd.exec_():
+            self.file = fsd.selectedFiles()[-1]
+            if self.file[-4:] != ".txt": self.file += ".txt"
+            self.save()
 
 app = QApplication(sys.argv)
 
 w = Window()
-mw = uic.loadUi("textedit.ui")
-mw.show()
-
-mw.brun.clicked.connect(w.run)
-mw.bstop.clicked.connect(w.stop)
+T = Textui()
 
 sys.exit(app.exec_())
